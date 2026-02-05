80dba7a8922fa766a87f0d3721022520dcc37460
Matheus Demeis <mfariasdemeis@my.bcit.ca>
2026-02-05 13:46:37 -0800

    adding backend skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,6 +1,6 @@
 export const skills = {
   'Frontend': ['React', 'Next.js', 'TypeScript', 'JavaScript', 'Tailwind CSS'],
-  'Backend': ['Node.js', 'Python', 'Java', 'C++', 'Express.js'],
+  'Backend': ['Node.js', 'Python', 'Java', 'C#', 'Express.js', 'PHP'],
   'Database': ['PostgreSQL', 'MongoDB', 'MySQL'],
   'Tools': ['GitHub', 'Vercel', 'Figma', 'Notion', 'zsh (bash)', "AWS"]
 };
