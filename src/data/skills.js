709d1858bea473cff28794a1e135fe34b96a4092
Matheus Demeis <mfariasdemeis@my.bcit.ca>
2026-02-26 22:48:07 -0800

    Cleanup unused assets and imports

--- src/data/skills.js
+++
@@ -1,6 +0,0 @@
-export const skills = {
-  'Frontend': ['React', 'Next.js', 'TypeScript', 'JavaScript', 'Tailwind CSS'],
-  'Backend': ['Node.js', 'C#', 'Java', 'Express.js', 'PHP', 'Python'],
-  'Database': ['PostgreSQL', 'MongoDB', 'MySQL'],
-  'Tools': ['GitHub', 'Vercel', 'Figma', 'Notion', 'zsh (bash)', "AWS"]
-};
